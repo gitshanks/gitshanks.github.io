64ee20abe6efe8c76c71de4b01a87189806ab139
Nishank Sharma <nishankatwork@gmail.com>
2022-10-10 21:25:38 -0600

    Fixed icon color

--- js/menu-color.js
+++ js/menu-color.js
@@ -1,18 +1,25 @@
 // Change Color Navbar Elements on Scroll
 
    $(window).scroll(function(){
-    if ($(this).scrollTop() > 350) {
+    if ($(this).scrollTop() > 925) {
        $('.icon-bar').addClass('opaque');
        $('.navbar-brand img').attr('src', 'img/logo-light.png');
-       $('i.fa').attr('style', 'color:000');
+       $('i.fa').attr('style', 'color:#fff');
     } else {
        $('.icon-bar').removeClass('opaque');
        $('.navbar-brand img').attr('src', 'img/logo.png');
-       $('.i.fa').attr('style', 'color:#fff');
+       $('.i.fa').attr('style', 'color:#000');
     }
-});
+}
+);
 
+   $(window).scroll(function(){
+    if ($(this).scrollTop() < 925) {
 
+       $('i.fa').attr('style', 'color:#000');
+   }
+}
+);
 
 $(function(){
     $(window).scroll(function(){
@@ -20,13 +27,18 @@ $(function(){
             $('#topbar, .cart-label').fadeOut('slow');
             $('.logo img')
                 .attr('src','img/logo.png');
-            $('i.fa').attr('style', 'color:#fff');
+            $('.i.fa').attr('style', 'color:000');
+           
+
+            
         }
         if($(this).scrollTop() < 100) {
             $('#logo, #topbar, .cart-label').fadeIn('fast');
             $('.logo img')
                 .attr('src','img/logo-light.png');
-            $('i.fa').attr('style', 'color:#000');
+             $('i.fa').attr('style', 'color:#000');
+            
+          
         }
     });
 });
