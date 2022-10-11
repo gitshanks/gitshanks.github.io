100c82181f87060da6f514fed0353d2b8bee4240
Nishank Sharma <nishankatwork@gmail.com>
2022-10-10 21:00:37 -0600

    Usability updates

--- js/menu-color.js
+++ js/menu-color.js
@@ -4,9 +4,11 @@
     if ($(this).scrollTop() > 350) {
        $('.icon-bar').addClass('opaque');
        $('.navbar-brand img').attr('src', 'img/logo-light.png');
+       $('i.fa').attr('style', 'color:000');
     } else {
        $('.icon-bar').removeClass('opaque');
        $('.navbar-brand img').attr('src', 'img/logo.png');
+       $('.i.fa').attr('style', 'color:#fff');
     }
 });
 
@@ -18,11 +20,13 @@ $(function(){
             $('#topbar, .cart-label').fadeOut('slow');
             $('.logo img')
                 .attr('src','img/logo.png');
+            $('i.fa').attr('style', 'color:#fff');
         }
         if($(this).scrollTop() < 100) {
             $('#logo, #topbar, .cart-label').fadeIn('fast');
             $('.logo img')
                 .attr('src','img/logo-light.png');
+            $('i.fa').attr('style', 'color:#000');
         }
     });
 });
